5bddbc426d8936f5dafe14d7691e33b47dac0316
AanyaMsh <aanyam007@gmail.com>
2025-11-27 14:45:36 +0530

    q90.c

---
+++ q90.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while (str[i] != '\0') {
+
+        // lowercase → uppercase
+        if (str[i] >= 'a' && str[i] <= 'z')
+            str[i] = str[i] - 32;
+
+        // uppercase → lowercase
+        else if (str[i] >= 'A' && str[i] <= 'Z')
+            str[i] = str[i] + 32;
+
+        i++;
+    }
+
+    printf("Toggled string: %s", str);
+
+    return 0;
+}
