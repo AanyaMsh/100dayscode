be074b7dd55eb92c36ffcdfa84f5d3280a44ca33
AanyaMsh <aanyam007@gmail.com>
2025-11-27 14:18:55 +0530

    q71.c

---
+++ q71.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &c);
+
+    int a[r][c];
+
+    // Read matrix
+    printf("Enter %d elements:\n", r * c);
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    // Print matrix
+    printf("Matrix is:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
