a6c9dc3cccf301c0dc4e68f79fe931307e53186f
AanyaMsh <aanyam007@gmail.com>
2025-11-27 14:22:43 +0530

    q75.c

---
+++ q75.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main() {
+    int r, c, i, j;
+
+    printf("Enter rows: ");
+    scanf("%d", &r);
+
+    printf("Enter columns: ");
+    scanf("%d", &c);
+
+    int a[r][c], b[r][c], sum[r][c];
+
+    printf("Enter elements of Matrix A:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of Matrix B:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Add matrices
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("Sum Matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
