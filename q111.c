cfedc0c564365703f671e4598f41bc8b4e4416fd
AanyaMsh <aanyam007@gmail.com>
2025-11-28 23:06:36 +0530

    q111.c

---
+++ q111.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, k, i, j, found;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    scanf("%d", &k);
+
+    for (i = 0; i <= n - k; i++) {
+        found = 0;
+        for (j = i; j < i + k; j++) {
+            if (arr[j] < 0) {
+                printf("%d ", arr[j]);
+                found = 1;
+                break;
+            }
+        }
+        if (!found)
+            printf("0 ");
+    }
+
+    return 0;
+}
