3cd84ceb382d9b083a0f3efd53c0d7c736c74d3e
AanyaMsh <aanyam007@gmail.com>
2025-11-28 11:39:35 +0530

    q98.c

---
+++ q98.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char s[200];
+    int i = 0, lastStart = 0;
+
+    scanf("%[^\n]", s);
+
+    if (s[0] != ' ')
+        printf("%c ", s[0]);
+
+    for (i = 1; s[i] != '\0'; i++) {
+        if (s[i] == ' ' && s[i+1] != ' ') {
+            printf("%c ", s[i+1]);
+            lastStart = i + 1;
+        }
+    }
+
+    printf("%s", &s[lastStart]);
+    return 0;
+}
