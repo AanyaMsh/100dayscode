94b3195e6e403af129161118ccf5cac2d4f6cc2f
AanyaMsh <aanyam007@gmail.com>
2025-11-30 14:55:31 +0530

    q146.c

---
+++ q146.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct Date {
+    int day, month, year;
+};
+
+struct Employee {
+    char name[50];
+    int id;
+    struct Date join;
+};
+
+int main() {
+    struct Employee e;
+
+    scanf("%s %d %d %d %d", e.name, &e.id,
+          &e.join.day, &e.join.month, &e.join.year);
+
+    printf("Name: %s\nID: %d\nJoined: %d-%d-%d",
+           e.name, e.id, e.join.day, e.join.month, e.join.year);
+
+    return 0;
+}
