9c44f4e8d9cf6bdc4ef8403ee2241479a5254e76
AanyaMsh <aanyam007@gmail.com>
2025-11-30 14:50:14 +0530

    q137.c

---
+++ q137.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+enum Role { ADMIN, USER, GUEST };
+
+int main() {
+    enum Role r;
+    scanf("%d", &r);
+
+    if (r == ADMIN)
+        printf("Welcome Admin");
+    else if (r == USER)
+        printf("Welcome User");
+    else if (r == GUEST)
+        printf("Welcome Guest");
+    else
+        printf("Invalid Role");
+
+    return 0;
+}
